a04c3da8ec5324a8529d4f6415e12af5b40a02ca
rowanabisaiutp <a.rowan.ojeda@utponiente.edu.mx>
2024-12-19 23:04:31 -0600

    si jala pa

---
+++ app-blazor--/Program.cs
@@ -0,0 +1,33 @@
+using app_blazor__.Client.Pages;
+using app_blazor__.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(app_blazor__.Client._Imports).Assembly);
+
+app.Run();
